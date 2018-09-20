3f2e735f7ee1a7af8d69648c94433958c59c8951
Thomas Weise <tweise@ustc.edu.cn>
2018-09-21 06:47:52 +0800

    shortening of selected code for book

--- src/main/java/aitoa/examples/jssp/JSSPRepresentationMapping.java
+++ src/main/java/aitoa/examples/jssp/JSSPRepresentationMapping.java
@@ -11,13 +11,11 @@ import aitoa.structure.IRepresentationMapping;
  */
 public final class JSSPRepresentationMapping implements
     IRepresentationMapping<int[], JSSPCandidateSolution> {
-
+// end relevant
   /**
    * a thread local with temporary variables: allow the use of
    * the objective function in multiple threads
    */
-// the definition of the inner class __TemporaryVariables is
-// omitted for the sake of brevity in the book
   private final ThreadLocal<__TemporaryVariables> m_temp;
 
   /**
@@ -39,7 +37,7 @@ public final class JSSPRepresentationMapping implements
     this.m_temp = ThreadLocal.withInitial(
         () -> new __TemporaryVariables(instance.m, instance.n));
   }
-
+// start relevant
   /**
    * Map a point {@code x} from the search space to a candidate
    * solution {@code y} in the solution space.
@@ -51,18 +49,18 @@ public final class JSSPRepresentationMapping implements
    */
   @Override
   public void map(final int[] x, final JSSPCandidateSolution y) {
-// get/create temporary variables
+// setup of temporary variables to all 0 omitted for brevity
+// end relevant
     final __TemporaryVariables temp = this.m_temp.get();
-// setup and initialize the temporary variables
     final int[] machineState = temp.m_machineState;
-    Arrays.fill(machineState, -1);
     final int[] machineTime = temp.m_machineTime;
-    Arrays.fill(machineTime, 0);
     final int[] jobState = temp.m_jobState;
-    Arrays.fill(jobState, -1);
     final int[] jobTime = temp.m_jobTime;
+    Arrays.fill(machineState, 0);
+    Arrays.fill(jobState, 0);
+    Arrays.fill(machineTime, 0);
     Arrays.fill(jobTime, 0);
-
+// start relevant
 // iterate over the jobs in the solution
     for (final int nextJob : x) {
 // get the definition of the steps that we need to take for
@@ -71,29 +69,27 @@ public final class JSSPRepresentationMapping implements
 // jobState tells us the index in this list for the next step to
 // do, but since the list contains machine/time pairs, we
 // multiply by 2 (by left-shifting by 1)
-      final int jobStep = (++jobState[nextJob]) << 1;
+      final int jobStep = (jobState[nextJob]++) << 1;
 
 // so we know the machine where the job needs to go next
       final int machine = jobSteps[jobStep]; // get machine
 
-// compute start time: it is either the next time when the
-// machine becomes idle or the time we have already spent on the
-// job - whatever happens later
+// start time is maximum of the next time when the machine
+// becomes idle and the time we have already spent on the job 
       final int start =
           Math.max(machineTime[machine], jobTime[nextJob]);
 // the end time is simply the start time plus the time the job
 // needs to spend on the machine
       final int end = start + jobSteps[jobStep + 1]; // end time
-// this holds for both the machine (it will become idle again
-// after end) and the job (it can go to the next station after
-// end)
+// it holds for both the machine (it will become idle after end)
+// and the job (it can go to the next station after end)
       jobTime[nextJob] = machineTime[machine] = end;
 
 // update the schedule with the data we have just computed
       final int[] schedule = y.schedule[machine];
-      schedule[++machineState[machine]] = nextJob;
-      schedule[++machineState[machine]] = start;
-      schedule[++machineState[machine]] = end;
+      schedule[machineState[machine]++] = nextJob;
+      schedule[machineState[machine]++] = start;
+      schedule[machineState[machine]++] = end;
     }
   }
 // end relevant
