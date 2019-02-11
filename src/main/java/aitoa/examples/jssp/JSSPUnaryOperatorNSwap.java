de36e48200f1dc3fa9c418cecb572e35331f9c25
Thomas Weise <tweise@ustc.edu.cn>
2019-02-11 12:27:38 +0800

    better documentation

--- src/main/java/aitoa/examples/jssp/JSSPUnaryOperatorNSwap.java
+++ src/main/java/aitoa/examples/jssp/JSSPUnaryOperatorNSwap.java
@@ -34,26 +34,26 @@ public final class JSSPUnaryOperatorNSwap
 
 // choose the index of the first sub-job to swap
     int i = random.nextInt(dest.length);
-    final int job_i = dest[i];
-    int last = job_i; // last stores the last job id to swap with
+    final int first = dest[i];
+    int last = first; // last stores the last job id to swap with
 
     boolean hasNext;
-    do { // we repeat a binomially distributed number of times
+    do { // we repeat a geometrically distributed number of times
       hasNext = random.nextBoolean();
       inner: for (;;) {// find a location with a different job
         final int j = random.nextInt(dest.length);
         final int job_j = dest[j];
         if ((last != job_j) && // don't swap job with itself
-            (hasNext || (job_i != job_j))) { // also not at end
+            (hasNext || (first != job_j))) { // also not at end
           dest[i] = job_j; // overwrite job at index i with job_j
           i = j; // remember index j: we will overwrite it next
           last = job_j; // but not with the same value job_j...
           break inner;
         }
       }
-    } while (hasNext); // binomial distribution
+    } while (hasNext); // Bernoulli process
 
-    dest[i] = job_i; // write back job_i to last copied index
+    dest[i] = first; // write back first id to last copied index
   }
 }
 // end relevant
