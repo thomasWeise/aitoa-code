efd42471161cc9708d9617b1b05f56608adb5b99
Thomas Weise <tweise@hfuu.edu.cn>
2019-12-14 09:58:45 +0800

    added trap function and version stepping

---
+++ src/main/java/aitoa/examples/bitstrings/TrapObjectiveFunction.java
@@ -0,0 +1,58 @@
+package aitoa.examples.bitstrings;
+
+/**
+ * The trap function basically is similar to the
+ * {@linkplain OneMaxObjectiveFunction OneMax problem}, except
+ * that it puts the global optimum at the all-0 string. However,
+ * following a trail of increasing fitness will still lead you to
+ * the all-1 string.
+ * <p>
+ * Droste, S., Jansen, T., and Wegener, I. (2002). On the
+ * analysis of the (1+1) evolutionary algo-rithm.Theoretical
+ * Computer Science, 276(1-2):51–81.
+ * https://doi.org/10.1016/S0304-3975(01)00182-7
+ */
+public final class TrapObjectiveFunction
+    extends BitStringObjectiveFunction {
+
+  /**
+   * create
+   *
+   * @param _n
+   *          the length of the bit string
+   */
+  public TrapObjectiveFunction(final int _n) {
+    super(_n);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public final double evaluate(final boolean[] y) {
+    int s = 0;
+    for (final boolean b : y) {
+      if (b) {
+        continue;
+      }
+      ++s;
+    }
+    return (s >= y.length) ? 0 : (s + 1);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public final double lowerBound() {
+    return 0;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public final double upperBound() {
+    return this.n + 1;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public final String toString() {
+    return "Trap_" + this.n; //$NON-NLS-1$
+  }
+}
---
+++ src/test/java/aitoa/examples/bitstrings/TestTrapObjectiveFunction.java
@@ -0,0 +1,61 @@
+package aitoa.examples.bitstrings;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import aitoa.TestTools;
+import aitoa.structure.IObjectiveFunction;
+import aitoa.structure.IObjectiveFunctionTest;
+
+/** A Test for the Trap Objective Function */
+public class TestTrapObjectiveFunction
+    extends IObjectiveFunctionTest<boolean[]> {
+
+  /** the onemax */
+  public static final TrapObjectiveFunction F =
+      new TrapObjectiveFunction(20);
+
+  /** {@inheritDoc} */
+  @Override
+  protected IObjectiveFunction<boolean[]> getInstance() {
+    return TestTrapObjectiveFunction.F;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected boolean[] createValid() {
+    final boolean[] x =
+        new boolean[TestTrapObjectiveFunction.F.n];
+    final Random r = ThreadLocalRandom.current();
+    for (int i = x.length; (--i) >= 0;) {
+      x[i] = r.nextBoolean();
+    }
+    return x;
+  }
+
+  /** test the correctness */
+  @SuppressWarnings("static-method")
+  @Test(timeout = 3600000)
+  public final void testCorrectness() {
+    final boolean[] x =
+        new boolean[TestTrapObjectiveFunction.F.n];
+
+    for (int i = 0; i <= x.length; i++) {
+      final int exp = (i == 0) ? 0 : ((x.length + 1) - i);
+
+      Assert.assertEquals(
+          TestTrapObjectiveFunction.F.evaluate(x), exp, 0);
+      TestTools.assertGreaterOrEqual(exp,
+          TestTrapObjectiveFunction.F.lowerBound());
+      TestTools.assertLessOrEqual(exp,
+          TestTrapObjectiveFunction.F.upperBound());
+      if (i >= x.length) {
+        break;
+      }
+      x[i] = true;
+    }
+  }
+}
